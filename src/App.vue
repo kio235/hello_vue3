a2dfd75e13fdd6b5e76cf6e050f1626174808dd4
kio <2357730691@qq.com>
2025-02-01 03:22:16 +0800

    01_编写App组件

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app">
+        <h1>
+            hello vue!
+        </h1>
+    </div>
+
+</template>
+
+<script lang="ts">
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+.app {
+    background-color: #42b883;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+    color: white;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// 引入create app用于创建应用
+import {createApp} from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
