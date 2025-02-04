b1222bd939d3e42b775281a44d3490b8bccef303
kio <2357730691@qq.com>
2025-02-04 21:29:52 +0800

    07_toRef

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,50 +1,76 @@
 <template>
     <div class="person">
-        <h2>name: {{ name }}</h2>
-        <h2>age: {{ age }}</h2>
-        <h2>address: {{ address }}</h2>
-        <button @click="showTel">check phone number</button>
-        <button @click="changeName">change name</button>
-        <button @click="changeAge">change age</button>
+        <h2>Car brand: {{ car.brand }}. Value: {{ myCarPrice }}k</h2>
+        <button @click="changeCarPrice()">LOWER!!</button>
     </div>
+    <br>
+    <h2>Game list:</h2>
+    <ul>
+        <li v-for="games in games" v-bind:key="games.id">{{ games.name }}</li>
+    </ul>
+    <button @click="upgradeGame('fqfqfewe1')">Upgrade CS!</button>
+    <h2>test value: {{ obj.a.b.c }}</h2>
+    <button @click="changeTestValue()">change this value</button>
 </template>
 
-<script lang="ts">
+
+<!-- <script lang="ts">
 export default {
     name: 'Person',
 }
-</script>
+</script> -->
+
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import { reactive, ref, toRef, toRefs } from 'vue'
 
-let name = ref("ZS");    //此时不是响应式
-let age = ref(18);
-let tel = '13888888888'
-let address = 'Tianjin'
+let car = reactive({ brand: "Benz", price: 100 })
+let games = reactive([
+    { id: 'fqfqfewe1', name: 'csgo' },
+    { id: 'fqfqfewe2', name: 'lol' },
+    { id: 'fqfqfewe3', name: 'dota' },
+])
 
-console.log(1,name)
-console.log(2,age)
-console.log(3,tel)
-console.log(5,address)
+let obj = reactive({
+    a:{
+        b:{
+            c:'666'
+        }
+    }
+})
 
-function changeName() {
-    name.value = "LS";
+let myCarPrice = toRef(car,'price')
+
+function changeCarPrice() {
+    car.price += 10;
 }
-function changeAge() {
-    age.value += 1;
+
+function upgradeGame(id:string) {
+    let game = games.find(game => game.id == id);
+    console.log(game)
+    if (game) {
+        game.name = 'cs2';
+    }
 }
-function showTel() {
-    alert(tel);
+
+function changeTestValue(){
+    obj.a.b.c = '999';
 }
+
 </script>
+
 
 <style scoped>
 .person {
     background-color: skyblue;
-    box-shadow: 0 0 10px;
+    /* box-shadow: 0 0 10px; */
     border-radius: 10px;
     padding: 20px;
     color: white;
+}
+
+li {
+    font-size: 20px;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 }
 </style>
