57c4bf411c261c123a10f57b739386b735ead291
kio <2357730691@qq.com>
2025-02-06 16:36:36 +0800

    09_watch情况1、2

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,17 +1,29 @@
 <template>
     <div class="person">
-        <h1><span class="red">Scenario 1</span>: Monitoring the <span class="red">basic data</span> type defined by ref.</h1>
+        <h1><span class="red">Scenario 1</span>: Monitoring the <span class="red">BASIC DATA</span> Type Defined by Ref.
+        </h1>
         <h2>sum: {{ sum }}</h2>
         <button @click="sumPlusOne()">Click me to plus one!</button>
+    </div>
+    <br>
+    <div class="person">
+        <h1><span class="red">Scenario 2</span>: Monitoring the <span class="red">OBJECT</span> Type Defined by Ref.
+        </h1>
+        <h2>name: {{ person.name }}</h2>
+        <h2>age: {{ person.age }}</h2>
+        <button @click="changePersonName()">change name</button>
+        <button @click="increasePersonAge()">increase age</button>
+        <button @click="changePerson()">change person</button>
+        <button @click="changePerson2()">change person2</button>
     </div>
 </template>
 
 
-<!-- <script lang="ts">
+<script lang="ts">
 export default {
     name: 'Person',
 }
-</script> -->
+</script>
 
 
 <script setup lang="ts">
@@ -23,12 +35,45 @@ function sumPlusOne() {
     sum.value += 1;
 }
 
-const stopWatch=watch(sum,(newval,oldval)=>{
-    console.log('sum has changed',newval,oldval);
-    if(newval>=10){
+const stopWatch = watch(sum, (newval, oldval) => {
+    console.log('sum has changed', newval, oldval);
+    if (newval >= 10) {
         stopWatch();
     }
 })
+
+/*--------------------------------------------------*/
+
+let person = ref({
+    name: 'Zhang San',
+    age: 18
+})
+
+function changePersonName() {
+    person.value.name = 'Tim Cook';
+}
+
+function increasePersonAge() {
+    person.value.age++;
+}
+
+function changePerson() {
+    person.value = { name: 'W ZZ', age: 23 }
+}
+
+function changePerson2() {
+    ({ name: person.value.name, age: person.value.age } = { name: 'LWW', age: 16 })
+}
+
+
+watch(
+    person,
+    (newval, oldval) => {
+        console.log(newval.age, oldval?.age)
+    },
+    { deep: true, immediate:true }
+)
+
 </script>
 
 
