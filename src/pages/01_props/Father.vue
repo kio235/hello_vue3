7891d4c9698b1d48deb8f107bcce572190042292
kio <2357730691@qq.com>
2025-03-22 10:56:31 +0800

    24_组件通信_方式1_props

--- src/pages/01_props/Father.vue
+++ src/pages/01_props/Father.vue
@@ -1,12 +1,26 @@
 <template>
 	<div class="father">
 		<h2>我是父组件</h2>
-		<son />
+		<h4>My car: {{ car }}</h4>
+		<h4 v-show="toy">My Son's Toy: {{ toy }}</h4>
+		<button @click="changeCar">change car</button>
+		<son :car="car" :sendToy="getSonToy" />
 	</div>
 </template>
 
 <script setup lang="ts" name="father">
+import { reactive, ref } from "vue";
 import Son from "./Son.vue";
+const car = ref("Benz");
+const toy = ref("");
+
+function changeCar() {
+	car.value += "S";
+}
+
+function getSonToy(sonToy: string) {
+	toy.value = sonToy;
+}
 </script>
 
 <style scoped>
--- src/pages/01_props/Son.vue
+++ src/pages/01_props/Son.vue
@@ -1,10 +1,36 @@
 <template>
 	<div class="child">
 		<h2>我是子组件</h2>
+		<h4>My Toy: {{ toy }}</h4>
+		<h4>Father's car: {{ props.car }}</h4>
+		<button @click="upgradeToy">upgrade toy</button>
 	</div>
 </template>
 
-<script setup lang="ts" name="child"></script>
+<script setup lang="ts" name="child">
+import { onMounted, ref, watch } from "vue";
+
+const props = defineProps({
+	car: String,
+	sendToy: {
+		type: Function,
+		required: true,
+	},
+});
+
+const toy = ref("PS5");
+
+function upgradeToy() {
+	toy.value += "Pro";
+}
+
+onMounted(() => {
+	props.sendToy(toy.value);
+});
+watch(toy, (newval) => {
+	props.sendToy(newval);
+});
+</script>
 
 <style scoped>
 .child {
