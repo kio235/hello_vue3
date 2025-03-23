e89bad9100fd455161449fe575fe204923d304d1
kio <2357730691@qq.com>
2025-03-23 11:07:41 +0800

    26_组件通信_方式4_v-model

--- src/pages/04_v-model/Father.vue
+++ src/pages/04_v-model/Father.vue
@@ -1,10 +1,33 @@
 <template>
 	<div class="father">
 		<h2>我是父组件</h2>
+		<!-- v-model用在HTML标签上 -->
+		<!-- <input type="text" v-model="userName" /> -->
+		<!-- <input
+			type="text"
+			:value="userName"
+			@input="userName = ($event.target as HTMLInputElement).value"
+		/> -->
+
+		<!-- v-model用在组件上 -->
+		<!-- <MyInput
+			:modelValue="userName"
+			@update:modelValue="userName = $event.value"
+		></MyInput> -->
+
+		<MyInput v-model="userName" />
+
+		<h4>user name: {{ userName }}</h4>
 	</div>
 </template>
 
-<script setup lang="ts" name="father"></script>
+<script setup lang="ts" name="father">
+import { ElCountdown, ElInput } from "element-plus";
+import { ref } from "vue";
+import MyInput from "./MyInput.vue";
+
+const userName = ref<string>("default user name");
+</script>
 
 <style scoped>
 .father {
---
+++ src/pages/04_v-model/MyInput.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<input
+			type="text"
+			:value="props.modelValue"
+			@input="
+				emits(
+					'update:modelValue',
+					($event.target as HTMLInputElement).value
+				)
+			"
+		/>
+	</div>
+</template>
+
+<script setup lang="ts" name="MyInput">
+const props = defineProps({
+	modelValue: {
+		type: String,
+	},
+});
+
+const emits = defineEmits(["update:modelValue"]);
+</script>
+
+<style scoped>
+input {
+	background-color: whitesmoke;
+	border-color: whitesmoke;
+	border-radius: 5px;
+	border: 2px;
+	padding: 5px;
+	height: 20px;
+	width: 150px;
+}
+</style>
